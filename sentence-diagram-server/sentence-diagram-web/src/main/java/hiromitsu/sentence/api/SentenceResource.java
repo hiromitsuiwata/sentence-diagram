0c49748cdb70113148ab08cf0827243530c26c3a
Hiromitsu Iwata <hiromitsu7@gmail.com>
2018-12-25 00:52:46 +0900

    add vue, axios

--- sentence-diagram-server/sentence-diagram-web/src/main/java/hiromitsu/sentence/api/SentenceResource.java
+++ sentence-diagram-server/sentence-diagram-web/src/main/java/hiromitsu/sentence/api/SentenceResource.java
@@ -1,25 +1,56 @@
 package hiromitsu.sentence.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import hiromitsu.sentence.ParsedResult;
 
 /**
  * 文を扱うリソースクラス
  */
-@Path("sentence")
+@Path("sentences")
 public class SentenceResource {
+	
+	private static List<String> sentences = new ArrayList<>();
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@GET
 	@Produces("application/json")
-	public Response getSentence() {
-
+	public Response getSentences() {
 		ParsedResult result = new ParsedResult();
-		result.setOriginalSentence("test data");
-
-		return Response.ok(result).build();
+		result.setOriginalSentence(sentences.toString());
+		Response response = Response.ok(result).build();
+		logger.info(response.toString());
+		return response;
+	}
+	
+	@GET
+	@Produces("application/json")
+	@Path("{id}")
+	public Response getSentence(@PathParam("id") int id) {
+		ParsedResult result = new ParsedResult();
+		result.setOriginalSentence(sentences.get(id));
+		Response response = Response.ok(result).build();
+		logger.info("getSentence");
+		return response;
+	}
+	
+	@POST
+	public Response createSentence(@FormParam("text") String text) {
+		sentences.add(text);
+		Response response = Response.ok().build();
+		logger.info(response.toString());
+		return response;
 	}
 }
---
+++ sentence-diagram-server/sentence-diagram-web/src/main/webapp/js/sentences.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  },
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios.get('https://localhost:9443/sentence-diagram-web/api/sentences')
+    .then(response => this.info = response.data.originalSentence);
+  },
+  methods: {
+    create: function() {
+      var params = new URLSearchParams();
+      params.append('text', document.getElementsByName('text')[0].value);
+      axios.post('/sentence-diagram-web/api/sentences', params);
+      document.getElementsByName('text')[0].value = '';
+      axios.get('https://localhost:9443/sentence-diagram-web/api/sentences')
+      .then(response => this.info = response.data.originalSentence);
+    }
+  }
+});
