feca30551b44452c92afbbbdbf37f80d5e54cc47
Hiromitsu Iwata <hiromitsu7@gmail.com>
2019-02-16 13:55:33 +0900

    add eslint

---
+++ sentence-diagram-client/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
