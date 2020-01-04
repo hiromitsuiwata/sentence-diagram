2c555735e4e6a6e7af7ac80dbd6dcd1ca1081ede
Hiromitsu Iwata <hiromitsu7@gmail.com>
2020-01-04 23:19:30 +0900

    サンプルsvg追加

--- react-client/src/pages/home/mycard/text/MyCardText.tsx
+++ react-client/src/pages/home/mycard/text/MyCardText.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const MyCardText = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
+    <>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 300" width="400" height="300"></svg>
+    </>
   );
 };
 
