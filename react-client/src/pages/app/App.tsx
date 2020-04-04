94dcd7b9b67d9fc855913ddcaa80224c1c7967da
Hiromitsu Iwata <hiromitsu7@gmail.com>
2020-04-05 01:01:55 +0900

    見た目調整

--- react-client/src/pages/app/App.tsx
+++ react-client/src/pages/app/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../home/Home';
-import Register from '../register/Register';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div>
         <Route exact path="/" component={Home} />
-        <Route path="/register" component={Register} />
       </div>
     </BrowserRouter>
   );
--- react-client/src/pages/home/card/Card.tsx
+++ react-client/src/pages/home/card/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilRuler } from '@fortawesome/free-solid-svg-icons';
+import { faPencilRuler, faExternalLinkAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Card.module.css';
 
@@ -40,18 +40,18 @@ class Card extends React.Component<Props, State> {
           <div className={styles.id}>#{this.props.id}</div>
           <div className={styles.link}>
             <a target="_blank" rel="noopener noreferrer" href={this.props.url}>
-              link
+              <FontAwesomeIcon icon={faExternalLinkAlt} />
             </a>
           </div>
           <div className={styles.diagram}>
-            <a href="#top" onClick={this.showDiagram}>
-              diagram
+            <a href="/" onClick={this.showDiagram}>
+              diagram&nbsp;
               <FontAwesomeIcon icon={faPencilRuler} />
             </a>
           </div>
-          <div className={styles.garbage}>
-            <a href="#top" onClick={this.delete}>
-              drop
+          <div className={styles.trash}>
+            <a href="/" onClick={this.delete}>
+              <FontAwesomeIcon icon={faTrashAlt} />
             </a>
           </div>
         </div>
