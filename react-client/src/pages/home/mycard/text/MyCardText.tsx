fc953d04178ef6a865727412b98f0c8479b671ab
Hiromitsu Iwata <hiromitsu7@gmail.com>
2020-06-28 06:13:21 +0900

    サンプルを変更

--- react-client/src/pages/home/mycard/text/MyCardText.tsx
+++ react-client/src/pages/home/mycard/text/MyCardText.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import TextLine from '../../diagram/svg/TextLine';
+import Coordinate from '../../diagram/Coordinate';
+import Word from '../../diagram/Word';
 
 interface Props {}
 
-interface State {
-  starts: Start[];
-}
-
-class MyCardText extends React.Component<Props, State> {
+class MyCardText extends React.Component<Props> {
   private words: Word[] = [];
-  private tempStarts: Start[] = [];
+  private tempStarts: Coordinate[] = [];
+  private tempEnds: Coordinate[] = [];
 
   constructor(props: Props) {
     super(props);
 
-    this.state = {
-      starts: [],
-    };
-    for (let i = 0; i < 4; i++) {
-      this.state.starts.push({ x: 0, y: 0 });
-    }
-    // 最初のwordの始点
-    this.state.starts[0].x = 40;
-    this.state.starts[0].y = 40;
+    this.tempStarts.push({ x: 40, y: 40 });
+
     // word
     this.words.push({ id: 0, text: 'unicorn' });
     this.words.push({
@@ -51,53 +43,75 @@ class MyCardText extends React.Component<Props, State> {
     console.log(JSON.stringify(this.words));
   }
 
-  /**
-   * ある単語の長さの計算が終わったときに呼ばれるハンドラー
-   */
-  private handleCompute = (
-    lengthComputedWordId: number,
-    startX: number,
-    startY: number,
-    endX: number,
-    endY: number
-  ) => {
-    // 単語の長さを計算した結果を一時変数に保存してマウントされたときにstateに反映させる
-    // TODO 単語間の位置関係における制約を記述する
-    this.words.forEach((word) => {
-      if (lengthComputedWordId === word.parentId) {
-        if ('nsubj' === word.relation) {
-          // 親とnsubj関連を持つ場合、親の終点が子の始点となる
-          this.tempStarts[word.id] = { x: endX, y: startY };
-        } else if ('det' === word.relation || 'amod' === word.relation) {
-          if (typeof word.childOrder != 'undefined' && word.childOrder >= 0) {
-            // 親とdetまたはamod関連を持つ場合、親の単語の途中の位置が子の始点となる
+  private fillShortage = (): void => {
+    const startsShortage = this.words.length - this.tempStarts.length;
+    for (let i = 0; i < startsShortage; i++) {
+      this.tempStarts.push({ x: 0, y: 0 });
+    }
+    const startsEnds = this.words.length - this.tempEnds.length;
+    for (let i = 0; i < startsEnds; i++) {
+      this.tempEnds.push({ x: 0, y: 0 });
+    }
+  };
+
+  private computeEnds = (): void => {
+    for (let i = 0; i < this.words.length; i++) {
+      const length: number = this.words[i].text.length * 9 + 20;
+
+      if (this.words[i].direction === 'right-down') {
+        this.tempEnds[i].x = Math.round(this.tempStarts[i].x + length / Math.sqrt(2));
+        this.tempEnds[i].y = Math.round(this.tempStarts[i].y + length / Math.sqrt(2));
+      } else {
+        this.tempEnds[i].x = Math.round(this.tempStarts[i].x + length);
+        this.tempEnds[i].y = Math.round(this.tempStarts[i].y);
+      }
+    }
+  };
+
+  private computeStarts = (): void => {
+    for (let i = 0; i < this.words.length; i++) {
+      this.words.forEach((word) => {
+        if (i === word.parentId) {
+          if ('nsubj' === word.relation) {
+            // 親とnsubj関連を持つ場合、親の終点が子の始点となる
             this.tempStarts[word.id] = {
-              x: startX + 20 * (word.childOrder + 1),
-              y: startY,
+              x: this.tempEnds[i].x,
+              y: this.tempStarts[i].y,
             };
+          } else if ('det' === word.relation || 'amod' === word.relation) {
+            if (typeof word.childOrder != 'undefined' && word.childOrder >= 0) {
+              // 親とdetまたはamod関連を持つ場合、親の単語の途中の位置が子の始点となる
+              this.tempStarts[word.id] = {
+                x: this.tempStarts[i].x + 20 * (word.childOrder + 1),
+                y: this.tempStarts[i].y,
+              };
+            }
           }
         }
-      }
-
-      if (lengthComputedWordId === word.id && word.parentId === undefined) {
-        // 自分自身の始点(本来意味がないがReactの性質のためstateを丸ごと入れ替えているので記述必要)
-        this.tempStarts[word.id] = { x: startX, y: startY };
-      }
-    });
+      });
+    }
   };
 
   render(): JSX.Element {
-    // 固定部分(words)と可変部分(state.starts)を使ってタグを作る
+    // tempStarts, tempEndsの不足部分を埋める
+    this.fillShortage();
+    // 始点と単語の長さから終点を決定する
+    this.computeEnds();
+    // 単語間の意味の関連から始点を変更する
+    this.computeStarts();
+    // 変更した始点に対して再度終点を決定する
+    this.computeEnds();
+
     const list = [];
     for (let i = 0; i < this.words.length; i++) {
       list.push(
         <TextLine
           key={this.words[i].id}
           wordId={this.words[i].id}
-          startX={10}
-          startY={10}
-          endX={10}
-          endY={10}
+          startX={this.tempStarts[i].x}
+          startY={this.tempStarts[i].y}
+          endX={this.tempEnds[i].x}
+          endY={this.tempEnds[i].y}
           text={this.words[i].text}
           separator={this.words[i].separator}
           direction={this.words[i].direction}
@@ -122,11 +136,7 @@ class MyCardText extends React.Component<Props, State> {
 
 export default MyCardText;
 
-class Start {
-  x: number = 0;
-  y: number = 0;
-}
-
+/*
 class Word {
   id: number = 0;
   text: string = '';
@@ -136,3 +146,4 @@ class Word {
   relation?: string = '';
   childOrder?: number = -1;
 }
+*/
