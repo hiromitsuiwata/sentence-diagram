178a13561e1b3ab65473d1f8466e8728734ca253
Hiromitsu Iwata <hiromitsu7@gmail.com>
2020-07-30 20:21:39 +0900

    色変更

--- react-client/src/pages/home/diagram/Diagram.tsx
+++ react-client/src/pages/home/diagram/Diagram.tsx
@@ -25,7 +25,7 @@ interface State {
 const override = css`
   display: block;
   margin: 0 auto;
-  border-color: #0bd;
+  border-color: #2182c4;
 `;
 
 class Diagram extends React.Component<Props, State> {
@@ -223,7 +223,7 @@ class Diagram extends React.Component<Props, State> {
                 <BounceLoader
                   css={override}
                   size={60}
-                  color={'#0bd'}
+                  color={'#2182C4'}
                   loading={this.state.loading}
                 />
               </div>
