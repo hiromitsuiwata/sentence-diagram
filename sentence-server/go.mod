1ece4a589abaadc818eeb2015ba651607ee07392
Hiromitsu Iwata <hiromitsu7@gmail.com>
2020-08-06 00:21:44 +0900

    goでCRUDのAPIを作る

--- sentence-server/go.mod
+++ sentence-server/go.mod
@@ -3,12 +3,11 @@ module github.com/hiromitsu7/sentence-diagram/sentence-server
 go 1.14
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/labstack/echo/v4 v4.1.16
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/valyala/fasttemplate v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
-	golang.org/x/sys v0.0.0-20200803150936-fd5f0c170ac3 // indirect
+	golang.org/x/sys v0.0.0-20200805065543-0cf7623e9dbd // indirect
 	golang.org/x/text v0.3.3 // indirect
 )
--- sentence-server/main.go
+++ sentence-server/main.go
@@ -2,25 +2,60 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
+type (
+	sentence struct {
+		ID    int    `json:"id"`
+		Title string `json:"title"`
+		Text  string `json:"text"`
+		URL   string `json:"url"`
+	}
+)
+
+var (
+	// DBの代わりにmapを使う
+	sentences = map[int]*sentence{}
+	seq       = 1
+)
 
 func main() {
 	e := echo.New()
-	e.GET("/user", show)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/sentences", getSentences)
+	e.DELETE("/sentences/:id", deleteSentence)
+	e.POST("/sentences", createSentence)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func show(c echo.Context) error {
-	u := new(User)
-	if err := c.Bind(u); err != nil {
+func createSentence(c echo.Context) error {
+	s := &sentence{
+		ID:    seq,
+		Title: c.FormValue("title"),
+		Text:  c.FormValue("text"),
+		URL:   c.FormValue("url"),
+	}
+	if err := c.Bind(s); err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK, u)
+	sentences[s.ID] = s
+	seq++
+	return c.JSON(http.StatusCreated, s)
+}
+
+func getSentences(c echo.Context) error {
+	return c.JSON(http.StatusOK, sentences)
+}
+
+func deleteSentence(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(sentences, id)
+	return c.NoContent(http.StatusNoContent)
 }
